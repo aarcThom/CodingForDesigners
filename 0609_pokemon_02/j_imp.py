c748674b6ef33538f9758df114903bf95d5cbe2f
tgaudin <tgaudin@DESKTOP-Q0BKV8N>
2025-06-16 18:22:49 -0700

    Cleanup pokemon lectures

--- 0609_pokemon_02/j_imp.py
+++
@@ -1,6 +0,0 @@
-import json # import the json library
-
-def import_json():
-    with open("data/pokemon.json", mode="r", encoding="utf-8") as context_manager:
-        pokedex = json.load(context_manager)
-        return pokedex
